05271eaa3bfb1295c37414d53f9c173bdc25d3df
muneobu <muneobu@gmail.com>
2023-08-29 23:26:59 +0900

    😅

--- tomato/js/login.js
+++ tomato/js/login.js
@@ -1,1 +1,14 @@
-console.log("potato");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", login);
+
+function login() {
+  event.preventDefault();
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+  console.log(req);
+}
